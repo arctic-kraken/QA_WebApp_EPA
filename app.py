0bef61b562336ed336f3a92241bb150b8f796c8c
Jakub Kiepas <jkiepas@nolanbusinesssolutions.com>
2026-02-06 17:28:27 +0000

    Playing around with Jinja

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
-from flask import Flask
+import flask
+from flask import render_template
+# from flask import Flask
 
 app = flask.Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return render_template('App/Landing.html')
+
+@app.route('/Dashboard')
+def dashboard():
+    return render_template('App/Dashboard.html') # just testing stuff :D
