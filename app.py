bef44370baf3d34e9a1f44d739a31318952626d7
Jakub Kiepas <jkiepas@nolanbusinesssolutions.com>
2026-03-05 15:48:40 +0000

    more changes to try to get it deployed

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from routes.user_bp import user_bp
 from routes.account_bp import account_bp
 from routes.statement_bp import statement_bp
 from routes.budget_bp import budget_bp
+from werkzeug.exceptions import NotFound, HTTPException
 import os
 
 # https://plainenglish.io/blog/flask-crud-application-using-mvc-architecture
@@ -17,7 +18,7 @@ app = flask.Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = f"mssql+pyodbc:///?odbc_connect={os.environ.get('DB_CONNECT_URL')}"
 # app.config.from_object('config') # look into this type of config
 
-app.config['FLASK_DEBUG'] = True
+# app.config['FLASK_DEBUG'] = True
 app.secret_key = os.environ.get('SECRET_KEY')
 app.app_context().push()
 # db = db(app) # apparently does same as line above
@@ -25,24 +26,20 @@ db.init_app(app)
 db.create_all()
 migrate = Migrate(app, db) # CONTINUE TRYING TO DO MIGRATIONS; do the live db upgrade with a production db uri
 
-with app.app_context():
-    try:
-        db.engine.connect()
-        print("DB connected")
-    except Exception as e:
-        print(f"DB connection failed: {e}")
-
-
-
 app.register_blueprint(app_bp)
 app.register_blueprint(user_bp, url_prefix="/user")
 app.register_blueprint(account_bp, url_prefix="/account")
 app.register_blueprint(statement_bp, url_prefix="/statement")
 app.register_blueprint(budget_bp, url_prefix="/budget")
 
-# user = db.session.get(User, 1)
-# print(f"{user.name} {user.password}")
+@app.errorhandler(NotFound)
+def page_not_found(e: HTTPException):
+    return '', 404
 
-
-
+with app.app_context():
+    try:
+        db.engine.connect()
+        print("DB connected")
+    except Exception as e:
+        print(f"DB connection failed: {e}")
 
